6e3afb1170509330d77c1bd5e237a47e1f6305fa
Monynich Kiem <64664561+hacker41601@users.noreply.github.com>
2022-03-29 11:26:03 -0400

    Create tiny.py

---
+++ tiny.py
@@ -0,0 +1,5 @@
+#Author: Monynich Kiem
+#Purpose: Text generation using RNN (recurrent neural networks) to generate text using Tiny Shakespeare Text
+#Date: 03/29/2022
+#Libraries are allowed for this, the implementation of RNN is what we need to do on our own
+#Following Youtube Tutorial?
